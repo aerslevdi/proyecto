3d36e8e33c7cd836d27bc72bea4698d450d5c45e
raviolex <tomasgonzlp@gmail.com>
2018-06-14 13:58:01 -0300

    sql

--- ttt.sql
+++ ttt.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `publicacion`
 --
-
+create database isoldb;
+use isoldb;
 DROP TABLE IF EXISTS `publicacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
